94104cc3fdfb4f7bfaa21223ef6d35d832cfabe4
- <->
2019-07-11 17:56:28 +0800

    .

--- kad.go
+++ kad.go
@@ -17,48 +17,15 @@ type bucket [BucketSize]*net.UDPAddr
 type kad struct {
 	id     uint32
 	bucket [MaxDepth]bucket
-	depth  uint16
 	Node   map[*net.UDPAddr][32]byte
 }
 
 func New(id [32]byte) *kad {
-	return &kad{id: murmur3.Sum32(id[:]), depth: 0}
+	return &kad{id: murmur3.Sum32(id[:])}
 }
 
 func (k *kad) depthIsFull(n uint16) bool {
 	return k.bucket[n][BucketSize-1] != nil
-}
-
-func (k *kad) split() bool {
-	nowDepth := bucket{}
-	nowPos := 0
-	next := bucket{}
-	nextPos := 0
-
-	kDepth := k.depth
-
-	if kDepth == MaxDepth {
-		return false
-	}
-	bucket := k.bucket[kDepth]
-	for _, addr := range bucket {
-		if k.Distance(k.Node[addr]) > kDepth {
-			next[nextPos] = addr
-			nextPos++
-		} else {
-			nowDepth[nowPos] = addr
-			nowPos++
-		}
-	}
-	if nextPos > 0 {
-		//TODO 回调获取更多的nowDepth
-		k.bucket[kDepth] = nowDepth
-		k.depth++
-		k.bucket[k.depth] = next
-		return true
-	} else {
-		return false
-	}
 }
 
 func (k *kad) Distance(id [32]byte) uint16 {
@@ -68,7 +35,7 @@ func (k *kad) Distance(id [32]byte) uint16 {
 
 func (k *kad) String() string {
 	b := strings.Builder{}
-	for i := uint16(0); i < k.depth; i++ {
+	for i := uint16(0); i < MaxDepth; i++ {
 		b.WriteString(fmt.Sprintf("%d :", i))
 		for _, node := range k.bucket[i] {
 			if node == nil {
@@ -83,27 +50,18 @@ func (k *kad) String() string {
 }
 
 func (k *kad) AddNode(id [32]byte, addr *net.UDPAddr) bool {
-	hashDepth := k.Distance(id)
-	depth := hashDepth
+	depth := k.Distance(id)
 
-	kDepth := k.depth
-	if depth > kDepth {
-		if k.depthIsFull(kDepth) {
-			if !k.split() {
-				return false
+	t := k.bucket[depth]
+
+	if t[BucketSize-1] == nil {
+		for i := range t {
+			if t[i] == nil {
+				t[i] = addr
+				return true
 			}
 		}
-		depth = k.depth
 	}
-	t := k.bucket[depth]
-	if t[BucketSize-1] != nil {
-		return false
-	}
-	for i := range t {
-		if t[i] == nil {
-			t[i] = addr
-			return true
-		}
-	}
+
 	return false
 }
