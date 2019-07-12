e033f56f99347bd34d9c4bc1c21f4b01d46f1efa
- <->
2019-07-12 17:27:07 +0800

    .

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/u6du/kad
 
 go 1.12
 
-require github.com/spaolacci/murmur3 v1.1.0 // indirect
+require (
+	github.com/spaolacci/murmur3 v1.1.0
+	github.com/u6du/trie v1.0.1
+)
--- kad.go
+++ kad.go
@@ -7,49 +7,28 @@ import (
 	"strings"
 
 	"github.com/spaolacci/murmur3"
+	"github.com/u6du/trie"
+	triebyte32 "github.com/u6du/trie/byte32"
 )
 
 const MaxDepth = 32
-const BucketSize = 24
+const BucketSize = 36
 
-type bucket [BucketSize]*net.UDPAddr
+type Addr [18]byte
+type Bucket [BucketSize]*net.UDPAddr
 
-type kad struct {
+type Kad struct {
 	id     uint32
-	bucket [MaxDepth]bucket
-	Node   map[*net.UDPAddr][32]byte
+	bucket [MaxDepth]Bucket
+	Trie   *triebyte32.Trie
 }
 
-func New(id [32]byte) *kad {
-	return &kad{id: murmur3.Sum32(id[:])}
+func New(id [32]byte) *Kad {
+	return &Kad{id: murmur3.Sum32(id[:]), Trie: trie.NewTrie()}
 }
 
-func (k *kad) depthIsFull(n uint16) bool {
-	return k.bucket[n][BucketSize-1] != nil
-}
+func (k *Kad) AddNode(id [32]byte, addr *net.UDPAddr) bool {
 
-func (k *kad) Distance(id [32]byte) uint16 {
-	hash := murmur3.Sum32(id[:])
-	return uint16(bits.OnesCount32(k.id ^ hash))
-}
-
-func (k *kad) String() string {
-	b := strings.Builder{}
-	for i := uint16(0); i < MaxDepth; i++ {
-		b.WriteString(fmt.Sprintf("%d :", i))
-		for _, node := range k.bucket[i] {
-			if node == nil {
-				break
-			}
-			b.WriteString(" ")
-			b.WriteString(node.String())
-		}
-		b.WriteString("\n")
-	}
-	return b.String()
-}
-
-func (k *kad) AddNode(id [32]byte, addr *net.UDPAddr) bool {
 	depth := k.Distance(id)
 
 	t := k.bucket[depth]
@@ -64,4 +43,29 @@ func (k *kad) AddNode(id [32]byte, addr *net.UDPAddr) bool {
 	}
 
 	return false
+}
+
+func (k *Kad) depthIsFull(n uint16) bool {
+	return k.bucket[n][BucketSize-1] != nil
+}
+
+func (k *Kad) Distance(id [32]byte) uint16 {
+	hash := murmur3.Sum32(id[:])
+	return uint16(bits.OnesCount32(k.id ^ hash))
+}
+
+func (k *Kad) String() string {
+	b := strings.Builder{}
+	for i := uint16(0); i < MaxDepth; i++ {
+		b.WriteString(fmt.Sprintf("%d :", i))
+		for _, node := range k.bucket[i] {
+			if node == nil {
+				break
+			}
+			b.WriteString(" ")
+			b.WriteString(node.String())
+		}
+		b.WriteString("\n")
+	}
+	return b.String()
 }
