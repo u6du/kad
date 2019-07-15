2000fa378f7760dcc087c6699ade25ba4dce74f3
- <->
2019-07-15 18:06:58 +0800

    .

--- kad.go
+++ kad.go
@@ -28,28 +28,49 @@ func hash(id [32]byte) uint32 {
 
 func New(id [32]byte) *Kad {
 	return &Kad{
-		id: hash(id),
-		Ip: *radixmapaddr.New(),
+		id:     hash(id),
+		Ip:     *radixmapaddr.New(),
+		bucket: [][]*addr.Addr{{}},
 	}
 }
 
 const SplitLen = 63
 
-func (k *Kad) add(now int, addr *addr.Addr) {
+func (k *Kad) add(now int, a *addr.Addr) {
+
 	length := len(k.bucket) - 1
 	if now > length {
-		if len(k.bucket) > SplitLen {
+		b := k.bucket[length]
+		bLen := len(b)
+		if bLen >= SplitLen {
+			next := []*addr.Addr{a}
+			var same []*addr.Addr
+			for i := 0; i < bLen; i++ {
+				if k.Distance(b[i].Id) > length {
+					next = append(next, b[i])
+				} else {
+					same = append(same, b[i])
+				}
+			}
+			k.bucket[length] = same
+			k.bucket = append(k.bucket, next)
 
+			return
 		} else {
 			now = length
 		}
 	}
-	k.bucket[now] = append(k.bucket[now], addr)
+	if len(k.bucket[now]) < SplitLen {
+		k.bucket[now] = append(k.bucket[now], a)
+	}
 }
 
 // Tree通过addr*的byte映射到 secret，通过id映射到addr*
 
 func (k *Kad) Add(id, secret [32]byte, udp *net.UDPAddr) bool {
+	k.lock.Lock()
+	defer k.lock.Unlock()
+
 	addrByte := udpaddr.Byte(udp)
 	addrExist, exist := k.Ip.Get(addrByte)
 	if exist {
@@ -59,13 +80,17 @@ func (k *Kad) Add(id, secret [32]byte, udp *net.UDPAddr) bool {
 			addrExist.Id = id
 			now := k.Distance(id)
 			if old != now {
+				length := len(k.bucket) - 1
+				if old > length {
+					old = length
+				}
 				bucketOld := k.bucket[old]
 				for i := range bucketOld {
 					if addrExist == bucketOld[i] {
 						k.bucket[old] = append(bucketOld[:i], bucketOld[i+1:]...)
 					}
 				}
-				k.bucket[now] = append(k.bucket[now], addrExist)
+				k.add(now, addrExist)
 			}
 		}
 		return false
@@ -87,7 +112,11 @@ func (k *Kad) Distance(id [32]byte) int {
 }
 
 func (k *Kad) String() string {
+	k.lock.RLock()
+	defer k.lock.RUnlock()
+
 	out := strings.Builder{}
+
 	for i := 0; i < len(k.bucket); i++ {
 		b := strings.Builder{}
 
@@ -104,6 +133,9 @@ func (k *Kad) String() string {
 			out.WriteString(s)
 			out.WriteString("\n")
 		}
+	}
+	if out.Len() == 0 {
+		return ""
 	}
 	return out.String()
 }
