753bfde24ce938dbc0a75b650697c062ae68270d
- <->
2019-07-16 10:29:48 +0800

    .

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/spaolacci/murmur3 v1.1.0
+	github.com/u6du/ex v0.0.0-20190711032711-9e0cd89c4843
 	github.com/u6du/radix v0.0.0-20190714141635-a2aaea908eda
 	github.com/u6du/udpaddr v0.0.0-20190713135946-d6324610170b
 )
--- kad_test.go
+++ kad_test.go
@@ -4,34 +4,46 @@ import (
 	"crypto/rand"
 	"net"
 	"testing"
+
+	"github.com/u6du/ex"
+	"github.com/u6du/udpaddr"
 )
 
 func TestKad_Add(t *testing.T) {
 	id := [32]byte{}
-	rand.Read(id[:])
+	_, err := rand.Read(id[:])
+	ex.Panic(err)
+
 	t.Logf("id = %x", id)
 	kad := New(id)
 	t.Logf("kad\n%s", kad)
 
 	var id2 [32]byte
 	for i := 0; i < 9999; i++ {
-		rand.Read(id2[:])
+		_, err = rand.Read(id2[:])
+		ex.Panic(err)
+
 		secret := [32]byte{}
-		rand.Read(secret[:])
-		ip := make([]byte, 4)
-		rand.Read(ip)
-		kad.Add(id2, secret, &net.UDPAddr{IP: ip, Port: 3232})
+
+		_, err = rand.Read(secret[:])
+		ex.Panic(err)
+
+		ip := make([]byte, 6)
+		_, err = rand.Read(ip)
+		ex.Panic(err)
+
+		kad.Add(id2, secret, udpaddr.Addr(ip))
 	}
 	t.Logf("kad\n%s", kad)
 
-	near := kad.LookUp(id)
-	if len(near) != 0 {
-		t.Error("lookup 自己的id应该是返回空列表（只返回和自己同样相似或者更加相似的节点）")
-	}
-
-	near = kad.LookUp(id2)
+	near := kad.LookUp(id2)
 	if len(near) <= 0 {
 		t.Error("应该能找到相似的节点")
+	}
+
+	near = kad.LookUp(id)
+	if len(near) != 0 {
+		t.Error("lookup 自己的id应该是返回空列表（只返回和自己同样相似或者更加相似的节点）")
 	}
 
 	total := 0
@@ -40,6 +52,18 @@ func TestKad_Add(t *testing.T) {
 	}
 	if uint(total) != kad.Len() {
 		t.Error("kad len != sum len(kad.bucket)")
+	}
+
+	var addr *net.UDPAddr
+
+	for i := range kad.bucket {
+		addr = kad.bucket[i][0].Udp
+		break
+	}
+
+	kad.Delete(addr)
+	if kad.Len() != uint(total-1) {
+		t.Error("Delete 应该删除一个地址")
 	}
 
 }
